71c7c135f4f34dbca36ccb8c8f02cc6fd64d0fec
Sihle (Bafo) Shabangu <82091092+Sihle-Shabangu@users.noreply.github.com>
2024-09-17 12:00:59 +0000

    create-passengers is now going through

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Home = () => {
   return (
     <div className="min-h-screen bg-gray-400 flex items-center justify-center">
-      <div className="bg-white p-10 rounded-lg shadow-md space-y-8">
+      <div className="bg-white p-10 rounded-lg shadow-md space-y-8 text-black">
         <h1 className="text-2xl font-bold text-center">Taxi DApp Forms</h1>
 
         <div className="grid grid-cols-1 gap-4">
